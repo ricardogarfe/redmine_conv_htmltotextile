e5f732a4c73346edde6fac00f8aed431f307be51
Ricardo García Fernández <ricardogarfe@gmail.com>
2012-12-05 16:35:12 +0100

    Fix routes.rb file for Redmine 2.x using rails 3.x format.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 #custom routes for this plugin
-ActionController::Routing::Routes.draw do |map|
-  map.with_options :controller => 'convert' do |convert_routes|
-    convert_routes.connect "convert/htmltotextile", :conditions => { :method => [:post, :put] }, :action => 'htmltotextile'
+if Rails::VERSION::MAJOR >= 3
+  RedmineApp::Application.routes.draw do
+  
+      match "convert/htmltotextile", :via => [:post, :put]
+  end
+else
+  ActionController::Routing::Routes.draw do |map|
+    map.with_options :controller => 'convert' do |convert_routes|
+      convert_routes.connect "convert/htmltotextile", :conditions => { :method => [:post, :put] }, :action => 'htmltotextile'
+    end
   end
 end
